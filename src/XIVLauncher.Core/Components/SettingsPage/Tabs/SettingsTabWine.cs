e93c432cd0228a61146486f97296aacc097eca65
Oro <93224879+orowith2os@users.noreply.github.com>
2022-08-09 15:06:08 -0500

    Update SettingsTabWine.cs

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabWine.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabWine.cs
@@ -24,7 +24,6 @@ public class SettingsTabWine : SettingsTab
                 CheckVisibility = () => startupTypeSetting.Value == WineStartupType.Custom
             },
 
-#if !FLATPAK
             new SettingsEntry<bool>("Enable Feral's GameMode", "Enable launching with Feral Interactive's GameMode CPU optimizations.", () => Program.Config.GameModeEnabled ?? true, b => Program.Config.GameModeEnabled = b)
             {
                 CheckVisibility = () => RuntimeInformation.IsOSPlatform(OSPlatform.Linux),
@@ -47,7 +46,6 @@ public class SettingsTabWine : SettingsTab
                     return null;
                 }
             },
-#endif
 
             new SettingsEntry<bool>("Enable DXVK ASYNC", "Enable DXVK ASYNC patch.", () => Program.Config.DxvkAsyncEnabled ?? true, b => Program.Config.DxvkAsyncEnabled = b),
             new SettingsEntry<bool>("Enable ESync", "Enable eventfd-based synchronization.", () => Program.Config.ESyncEnabled ?? true, b => Program.Config.ESyncEnabled = b),
